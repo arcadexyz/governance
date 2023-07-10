1c89f0cd1dab3525652f19c457be541c6d4cf01e
Mouzayan Delbourgo <mouzayan@arcade.xyz>
2023-07-10 18:57:23 +0100

    fix(omn-arc-08s): removed mulitply by -1 in event

--- contracts/ARCDVestingVault.sol
+++ contracts/ARCDVestingVault.sol
@@ -347,10 +347,10 @@ contract ARCDVestingVault is IARCDVestingVault, HashedStorageReentrancyBlock, Ba
 
         // get the change in voting power. voting power can only go down
         // since the sync is only called when tokens are claimed or grant revoked
-        int256 change = int256(grant.latestVotingPower) - int256(newVotingPower);
-        votingPower.push(grant.delegatee, delegateeVotes - uint256(change));
+        uint256 change = grant.latestVotingPower - newVotingPower;
+        votingPower.push(grant.delegatee, delegateeVotes - change);
 
-        emit VoteChange(grant.delegatee, who, -1 * change);
+        emit VoteChange(grant.delegatee, who, int256(change));
 
         grant.latestVotingPower = newVotingPower;
     }
