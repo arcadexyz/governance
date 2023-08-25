c9c0747ae97113a0105f60590442494223056360
PowVT <epowlowsky@gmail.com>
2023-08-25 19:08:11 -0400

    feat(deploy-gov): lint setup script

--- scripts/deploy/setup.ts
+++ scripts/deploy/setup.ts
@@ -165,28 +165,28 @@ export async function main(
     const tx11 = await nftBoostVault.setManager(MULTISIG);
     await tx11.wait();
     console.log("Transfer nftBoostVault timelock role to ArcadeCoreVoting...");
-    const tx42 = await nftBoostVault.setTimelock(arcadeCoreVoting.address);
-    await tx42.wait();
+    const tx12 = await nftBoostVault.setTimelock(arcadeCoreVoting.address);
+    await tx12.wait();
 
     // ================== ArcadeCoteVoting ==================
     console.log("Setting custom quorum thresholds in CoreVoting...");
     // ArcadeToken
-    const tx12 = await arcadeCoreVoting.setCustomQuorum(arcadeToken.address, MINT_TOKENS, MINT_TOKENS_QUORUM);
-    await tx12.wait();
-    const tx13 = await arcadeCoreVoting.setCustomQuorum(arcadeToken.address, SET_MINTER, SET_MINTER_QUORUM);
+    const tx13 = await arcadeCoreVoting.setCustomQuorum(arcadeToken.address, MINT_TOKENS, MINT_TOKENS_QUORUM);
     await tx13.wait();
+    const tx14 = await arcadeCoreVoting.setCustomQuorum(arcadeToken.address, SET_MINTER, SET_MINTER_QUORUM);
+    await tx14.wait();
     // NFTBoostVault
-    const tx14 = await arcadeCoreVoting.setCustomQuorum(
+    const tx15 = await arcadeCoreVoting.setCustomQuorum(
         nftBoostVault.address,
         SET_AIRDROP_CONTRACT,
         SET_AIRDROP_CONTRACT_QUORUM,
     );
-    await tx14.wait();
-    const tx15 = await arcadeCoreVoting.setCustomQuorum(nftBoostVault.address, UNLOCK, UNLOCK_QUORUM);
     await tx15.wait();
-    // Timelock
-    const tx16 = await arcadeCoreVoting.setCustomQuorum(timelock.address, REGISTER_CALL, REGISTER_CALL_QUORUM);
+    const tx16 = await arcadeCoreVoting.setCustomQuorum(nftBoostVault.address, UNLOCK, UNLOCK_QUORUM);
     await tx16.wait();
+    // Timelock
+    const tx17 = await arcadeCoreVoting.setCustomQuorum(timelock.address, REGISTER_CALL, REGISTER_CALL_QUORUM);
+    await tx17.wait();
     const tx18 = await arcadeCoreVoting.setCustomQuorum(timelock.address, SET_WAIT_TIME, SET_WAIT_TIME_QUORUM);
     await tx18.wait();
     // ArcadeTreasury
@@ -207,119 +207,120 @@ export async function main(
     );
     await tx22.wait();
     // OriginationController
-    const tx24 = await arcadeCoreVoting.setCustomQuorum(
+    const tx23 = await arcadeCoreVoting.setCustomQuorum(
         ORIGINATION_CONTROLLER_ADDR,
         SET_ALLOWED_VERIFIERS,
         SET_ALLOWED_VERIFIERS_QUORUM,
     );
-    await tx24.wait();
-    const tx25 = await arcadeCoreVoting.setCustomQuorum(
+    await tx23.wait();
+    const tx24 = await arcadeCoreVoting.setCustomQuorum(
         ORIGINATION_CONTROLLER_ADDR,
         SET_ALLOWED_PAYABLE_CURRENCIES,
         SET_ALLOWED_PAYABLE_CURRENCIES_QUORUM,
     );
-    await tx25.wait();
+    await tx24.wait();
 
     console.log("Decentralize ArcadeCoreVotings...");
-    const tx26 = await arcadeCoreVoting.authorize(arcadeGSCCoreVoting.address);
+    const tx25 = await arcadeCoreVoting.authorize(arcadeGSCCoreVoting.address);
+    await tx25.wait();
+    const tx26 = await arcadeCoreVoting.setOwner(timelock.address);
     await tx26.wait();
-    const tx27 = await arcadeCoreVoting.setOwner(timelock.address);
-    await tx27.wait();
 
     // ================= Timelock =================
     console.log("Decentralize Timelock...");
-    const tx28 = await timelock.deauthorize(DEPLOYER_ADDRESS);
+    const tx27 = await timelock.deauthorize(DEPLOYER_ADDRESS);
+    await tx27.wait();
+    const tx28 = await timelock.authorize(arcadeGSCCoreVoting.address);
     await tx28.wait();
-    const tx29 = await timelock.authorize(arcadeGSCCoreVoting.address);
+    const tx29 = await timelock.setOwner(arcadeCoreVoting.address);
     await tx29.wait();
-    const tx30 = await timelock.setOwner(arcadeCoreVoting.address);
-    await tx30.wait();
 
     // ================= ArcadeGSCCoreVoting =================
     console.log("Setting custom quorum thresholds in ArcadeGSCCoreVoting...");
     // LoanCore
-    const tx23 = await arcadeGSCCoreVoting.setCustomQuorum(LOAN_CORE_ADDR, PAUSE, PAUSE_QUORUM);
-    await tx23.wait();
+    const tx30 = await arcadeGSCCoreVoting.setCustomQuorum(LOAN_CORE_ADDR, PAUSE, PAUSE_QUORUM);
+    await tx30.wait();
     // timelock
-    const tx17 = await arcadeGSCCoreVoting.setCustomQuorum(timelock.address, INCREASE_TIME, INCREASE_TIME_QUORUM);
-    await tx17.wait();
-    console.log("Changing min lock time for GSC proposals from 3 days to 8 hours...");
-    const tx31 = await arcadeGSCCoreVoting.setLockDuration(GSC_MIN_LOCK_DURATION);
+    const tx31 = await arcadeGSCCoreVoting.setCustomQuorum(timelock.address, INCREASE_TIME, INCREASE_TIME_QUORUM);
     await tx31.wait();
-    console.log("Decentralize ArcadeGSCCoreVoting...");
-    const tx32 = await arcadeGSCCoreVoting.setOwner(timelock.address);
+    console.log("Changing min lock time for GSC proposals from 3 days to 8 hours...");
+    const tx32 = await arcadeGSCCoreVoting.setLockDuration(GSC_MIN_LOCK_DURATION);
     await tx32.wait();
+    console.log("Decentralize ArcadeGSCCoreVoting...");
+    const tx33 = await arcadeGSCCoreVoting.setOwner(timelock.address);
+    await tx33.wait();
 
     // ================= ArcadeTreasury =================
     console.log("Setting spend thresholds in ArcadeTreasury...");
-    const tx41 = await arcadeTreasury.setThreshold(arcadeToken.address, {
+    const tx34 = await arcadeTreasury.setThreshold(arcadeToken.address, {
         small: ARCD_SMALL,
         medium: ARCD_MEDIUM,
         large: ARCD_LARGE,
     });
-    await tx41.wait();
-    const tx43 = await arcadeTreasury.setThreshold(ETH_ADDRESS, {
+    await tx34.wait();
+    const tx35 = await arcadeTreasury.setThreshold(ETH_ADDRESS, {
         small: ETH_SMALL,
         medium: ETH_MEDIUM,
         large: ETH_LARGE,
     });
-    await tx43.wait();
-    const tx44 = await arcadeTreasury.setThreshold(WETH_ADDRESS, {
+    await tx35.wait();
+    const tx36 = await arcadeTreasury.setThreshold(WETH_ADDRESS, {
         small: WETH_SMALL,
         medium: WETH_MEDIUM,
         large: WETH_LARGE,
     });
-    await tx44.wait();
-    const tx45 = await arcadeTreasury.setThreshold(USDC_ADDRESS, {
+    await tx36.wait();
+    const tx37 = await arcadeTreasury.setThreshold(USDC_ADDRESS, {
         small: USDC_SMALL,
         medium: USDC_MEDIUM,
         large: USDC_LARGE,
     });
-    await tx45.wait();
-    const tx46 = await arcadeTreasury.setThreshold(USDT_ADDRESS, {
+    await tx37.wait();
+    const tx38 = await arcadeTreasury.setThreshold(USDT_ADDRESS, {
         small: USDT_SMALL,
         medium: USDT_MEDIUM,
         large: USDT_LARGE,
     });
-    await tx46.wait();
-    const tx47 = await arcadeTreasury.setThreshold(DAI_ADDRESS, {
+    await tx38.wait();
+    const tx39 = await arcadeTreasury.setThreshold(DAI_ADDRESS, {
         small: DAI_SMALL,
         medium: DAI_MEDIUM,
         large: DAI_LARGE,
     });
-    await tx47.wait();
-    const tx48 = await arcadeTreasury.setThreshold(WBTC_ADDRESS, {
+    await tx39.wait();
+    const tx40 = await arcadeTreasury.setThreshold(WBTC_ADDRESS, {
         small: WBTC_SMALL,
         medium: WBTC_MEDIUM,
         large: WBTC_LARGE,
     });
-    await tx48.wait();
-    const tx49 = await arcadeTreasury.setThreshold(APE_ADDRESS, {
+    await tx40.wait();
+    const tx41 = await arcadeTreasury.setThreshold(APE_ADDRESS, {
         small: APE_SMALL,
         medium: APE_MEDIUM,
         large: APE_LARGE,
     });
+    await tx41.wait();
 
     console.log("Grant ArcadeTreasury permissions to foundation multisig...");
-    const tx33 = await arcadeTreasury.grantRole(await arcadeTreasury.GSC_CORE_VOTING_ROLE(), FOUNDATION_MULTISIG);
-    await tx33.wait();
-    const tx34 = await arcadeTreasury.grantRole(await arcadeTreasury.CORE_VOTING_ROLE(), FOUNDATION_MULTISIG);
-    await tx34.wait();
-    const tx35 = await arcadeTreasury.grantRole(await arcadeTreasury.ADMIN_ROLE(), FOUNDATION_MULTISIG);
-    await tx35.wait();
-    const tx36 = await arcadeTreasury.renounceRole(await arcadeTreasury.ADMIN_ROLE(), DEPLOYER_ADDRESS);
-    await tx36.wait();
+    const tx42 = await arcadeTreasury.grantRole(await arcadeTreasury.GSC_CORE_VOTING_ROLE(), FOUNDATION_MULTISIG);
+    await tx42.wait();
+    const tx43 = await arcadeTreasury.grantRole(await arcadeTreasury.CORE_VOTING_ROLE(), FOUNDATION_MULTISIG);
+    await tx43.wait();
+    const tx44 = await arcadeTreasury.grantRole(await arcadeTreasury.ADMIN_ROLE(), FOUNDATION_MULTISIG);
+    await tx44.wait();
+    const tx45 = await arcadeTreasury.renounceRole(await arcadeTreasury.ADMIN_ROLE(), DEPLOYER_ADDRESS);
+    await tx45.wait();
 
     // ================= ReputationBadge =================
     console.log("Setup ReputationBadge manager roles...");
-    const tx37 = await reputationBadge.grantRole(await reputationBadge.BADGE_MANAGER_ROLE(), MULTISIG);
-    await tx37.wait();
-    const tx38 = await reputationBadge.grantRole(await reputationBadge.RESOURCE_MANAGER_ROLE(), MULTISIG);
-    await tx38.wait();
-    const tx39 = await reputationBadge.grantRole(await reputationBadge.ADMIN_ROLE(), MULTISIG);
-    await tx39.wait();
-    const tx40 = await reputationBadge.renounceRole(await reputationBadge.ADMIN_ROLE(), DEPLOYER_ADDRESS);
-    await tx40.wait();
+    const tx46 = await reputationBadge.grantRole(await reputationBadge.BADGE_MANAGER_ROLE(), MULTISIG);
+    await tx46.wait();
+    const tx47 = await reputationBadge.grantRole(await reputationBadge.RESOURCE_MANAGER_ROLE(), MULTISIG);
+    await tx47.wait();
+    const tx48 = await reputationBadge.grantRole(await reputationBadge.ADMIN_ROLE(), MULTISIG);
+    await tx48.wait();
+    const tx49 = await reputationBadge.renounceRole(await reputationBadge.ADMIN_ROLE(), DEPLOYER_ADDRESS);
+    await tx49.wait();
 }
 
 async function attachAddresses(jsonFile: string): Promise<ContractArgs> {
@@ -331,7 +332,6 @@ async function attachAddresses(jsonFile: string): Promise<ContractArgs> {
         if (!(key in jsonContracts)) continue;
 
         const argKey = jsonContracts[key];
-        // console.log(`Key: ${key}, address: ${jsonData[key]["contractAddress"]}`);
 
         const contract: Contract = await ethers.getContractAt(key, jsonData[key]["contractAddress"]);
 
